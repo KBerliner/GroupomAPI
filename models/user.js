6e4a70ab246abf2c7a851c6f54c541a4046cf683
Kyle <berlinerkyle@gmail.com>
2024-04-26 19:03:23 -0600

    Changed the arrow function to allow the use of the 'this' keyword.

--- models/user.js
+++ models/user.js
@@ -9,14 +9,17 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
-	lastLogin: { type: Date, required: true },
-	created: { type: Date, required: true },
+	role: { type: String, enum: ["admin"], default: "admin" },
+	lastLogin: { type: Date, default: Date.now },
+	created: { type: Date, default: Date.now },
+	updatedAt: { type: Date, default: Date.now },
 	profilePictureUrl: { type: String },
 });
 
 // Encrypting the Password before saving to the database
 
-userSchema.pre("save", async (next) => {
+userSchema.pre("save", async function (next) {
+	console.log("THIS", this);
 	if (!this.isModified("password")) return next();
 	this.password = await bcrypt.hash(this.password, 10);
 	next();
