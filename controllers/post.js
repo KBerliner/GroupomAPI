0ea0d39a81bbf33389e2773470ebb3185e27e3fb
Kyle <berlinerkyle@gmail.com>
2024-04-26 21:35:42 -0600

    Added the get all posts endpoint.

--- controllers/post.js
+++ controllers/post.js
@@ -54,4 +54,12 @@ exports.deletePost = async (req, res) => {
 	}
 };
 
-exports.getAllPosts = async (req, res) => {};
+exports.getAllPosts = async (req, res) => {
+	try {
+		const posts = await Post.find();
+		res.status(200).json(posts);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal server error." });
+	}
+};
