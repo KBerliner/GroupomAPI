962e962321972fe51d9c384f0dbc83591df3aa81
Kyle <berlinerkyle@gmail.com>
2024-04-26 19:34:50 -0600

    Added the option for a user to be of role 'user'.

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
 	username: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
-	role: { type: String, enum: ["admin"], default: "admin" },
+	role: { type: String, enum: ["admin", "user"], default: "user" },
 	lastLogin: { type: Date, default: Date.now },
 	created: { type: Date, default: Date.now },
 	updatedAt: { type: Date, default: Date.now },
