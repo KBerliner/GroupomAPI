1c0cb429d055b5fc25612929504578d460b29b70
Kyle <berlinerkyle@gmail.com>
2024-04-26 21:30:48 -0600

    Added the edit post endpoint.

--- controllers/post.js
+++ controllers/post.js
@@ -16,7 +16,26 @@ exports.createPost = async (req, res) => {
 	}
 };
 
-exports.editPost = async (req, res) => {};
+exports.editPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found." });
+		}
+		if (post.authorId !== req.user.id) {
+			return res.status(401).json({ error: "Unauthorized." });
+		}
+		post.title = req.body.title;
+		post.caption = req.body.caption;
+		post.imageUrl = req.body?.imageUrl;
+		post.likesEnabled = req.body.likesEnabled;
+		await post.save();
+		res.status(200).json({ success: true });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: "Internal server error." });
+	}
+};
 
 exports.deletePost = async (req, res) => {};
 
