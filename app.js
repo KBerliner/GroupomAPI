7ee5fe640d97f1b6fd60ff18323e1707df20c31c
Kyle <berlinerkyle@gmail.com>
2024-10-09 15:50:15 -0600

    Allowed API to work in PROD environment.

--- app.js
+++ app.js
@@ -30,12 +30,22 @@ db();
 
 // Setting up CORS
 
-app.use(
-	cors({
-		origin: "http://localhost:5173",
-		credentials: true,
-	})
-);
+if (import.meta.env.PROD) {
+	app.use(
+		cors({
+			origin:
+				"https://groupomernia-pi7qp87w1-kyles-projects-61d652f2.vercel.app/",
+			credentials: true,
+		})
+	);
+} else {
+	app.use(
+		cors({
+			origin: "http://localhost:5173",
+			credentials: true,
+		})
+	);
+}
 
 // Request Routing
 
